ef3830da741d983a602f58b222e8114229f9d48b
yuseonkim <yusun715@icloud.com>
2023-04-13 03:30:02 +0900

    week1(2)

---
+++ src/ch10/BirthDayTest.java
@@ -0,0 +1,13 @@
+package ch10;
+
+public class BirthDayTest {
+    public static void main(String[] args){
+        BirthDay date = new BirthDay();
+        date.setYear(1999);
+        date.setMonth(7);
+        date.setDay(15);
+
+        date.showDate();
+
+    }
+}
