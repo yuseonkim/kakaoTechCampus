f3c0bfd35e7af81f172ac15e9d8387b850b771ff
yuseonkim <yusun715@icloud.com>
2023-04-14 12:37:41 +0900

    week1(3)

---
+++ src/ch14/Bus.java
@@ -0,0 +1,21 @@
+package ch14;
+
+public class Bus {
+    int busNumber;
+    int passangerCount;
+    int money;
+
+
+    public Bus(int busNumber){
+        this.busNumber = busNumber;
+    }
+
+    public void take(int money){
+        this.money += money;
+        passangerCount++;
+    }
+
+    public void showBusInfo(){
+        System.out.println(busNumber+"의 승객수는 "+passangerCount+"명 이고 수입은 "+money+"원 입니다");
+    }
+}
