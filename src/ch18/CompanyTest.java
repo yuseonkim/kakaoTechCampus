a13fb962e449cd12ee0c8dd63595ac705f6fc932
yuseonkim <yusun715@icloud.com>
2023-04-15 22:41:31 +0900

    week1(마무리)

---
+++ src/ch18/CompanyTest.java
@@ -0,0 +1,12 @@
+package ch18;
+
+public class CompanyTest {
+
+    public static void main(String[] args){
+        Company company1 = Company.getInstance();
+        Company company2 = Company.getInstance();
+
+        System.out.println(company1+ " "+company2);
+
+    }
+}
